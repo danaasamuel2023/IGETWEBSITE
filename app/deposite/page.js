af7cf1f08bbc78fd220516edb402b9aea8ff7188
danaasamuel2023 <157542114+danaasamuel2023@users.noreply.github.com>
2025-03-21 03:19:28 -1200

    good

--- app/deposite/page.js
+++ app/deposite/page.js
@@ -1,7 +1,7 @@
 // pages/verify-payment.js
 'use client';
 import { useState, useEffect, Suspense } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import Head from 'next/head';
 import Link from 'next/link';
 
